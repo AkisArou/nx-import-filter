6cf84e05b4530987f38031833853fc2408533def
AkisArou <akisarou90@gmail.com>
2023-06-02 04:51:44 +0300

    add nx eslint rule

--- src/index.ts
+++ src/index.ts
@@ -2,65 +2,58 @@ import path from "path";
 import fs from "fs";
 import ts from "typescript/lib/tsserverlibrary";
 import { CommentObject, parse } from "comment-json";
+import { TSESLint } from "@typescript-eslint/utils";
+import * as parser from "@typescript-eslint/parser";
+import * as nxEslintPlugin from "@nx/eslint-plugin";
+import { Linter } from "@typescript-eslint/utils/dist/ts-eslint";
 
 type ProjectJsonPath = string & { __type: "project-json-path" };
+
+function log(info: ts.server.PluginCreateInfo, text: string) {
+  info.project.projectService.logger.info(`NX import filter: ${text}`);
+}
 
 function init(modules: { typescript: typeof ts }) {
   const ts = modules.typescript;
 
   function create(info: ts.server.PluginCreateInfo) {
-    info.project.projectService.logger.info("Initializing nx-import-filter");
+    log(info, "Initializing nx-import-filter");
 
     const { paths, baseUrl } = info.project.getCompilerOptions();
 
     if (!paths) {
-      info.project.projectService.logger.info("No paths configured");
+      log(info, "No paths configured");
       return;
     }
 
     if (!baseUrl) {
-      info.project.projectService.logger.info("No baseUrl configured");
+      log(info, "No baseUrl configured");
       return;
     }
 
     const workspaceRoot = ts.sys.resolvePath(baseUrl);
-    ts.parseJsonText;
+
+    log(info, `Workspace root -> ${workspaceRoot}`);
 
     const eslintConfig = parse(
       fs.readFileSync(path.join(workspaceRoot, ".eslintrc.json"), "utf-8")
-    ) as {
-      overrides?: {
-        files: string;
-        rules: Record<
-          string,
-          [
-            string,
-            {
-              allow: string[];
-              depConstraints: {
-                sourceTag: string;
-                onlyDependOnLibsWithTags: string[];
-              }[];
-            }
-          ]
-        >;
-      }[];
-    };
+    ) as Linter.Config;
+
+    log(info, "Found eslint config");
 
     const eslintRule = "@nx/enforce-module-boundaries";
-    const enforceModuleBoundariesOverride = eslintConfig?.overrides?.find(
-      (override) => override.rules[eslintRule]
-    );
+    // const enforceModuleBoundariesOverride = eslintConfig?.overrides?.find(
+    //   (override) => override?.rules?.[eslintRule]
+    // );
 
-    if (!enforceModuleBoundariesOverride) {
-      info.project.projectService.logger.info(
-        "No @nx/enforce-module-boundaries configured"
-      );
-      return;
-    }
+    // if (!enforceModuleBoundariesOverride) {
+    //   log(info, "No @nx/enforce-module-boundaries configured");
+    //   return;
+    // }
 
-    const [boundaryFiles, boundaries] =
-      enforceModuleBoundariesOverride.rules[eslintRule];
+    // const rrr = enforceModuleBoundariesOverride.rules![eslintRule];
+
+    // const [boundaryFiles, boundaries] = rrr;
 
     const tagsMap = new Map<
       ProjectJsonPath,
@@ -78,9 +71,12 @@ function init(modules: { typescript: typeof ts }) {
         fs.readFileSync(projectJsonPath, "utf-8")
       ) as CommentObject;
 
+      log(info, `Found project.json path for path -> ${key}`);
+
       const tags = projectJson.tags as string[] | undefined;
 
       if (!tags) {
+        log(info, `No tags for -> ${key}`);
         continue;
       }
 
@@ -89,11 +85,6 @@ function init(modules: { typescript: typeof ts }) {
         source: key,
       });
     }
-
-    tagsMap.forEach((val, key) => {
-      console.log("TAGSSS", key, JSON.stringify(val));
-      //Info 25   [22:20:18.266] TAGSSS, /home/akisarou/nx-testing/libs/ts-lib/project.json, {"tags":["app:assistant-volunteer"],"source":"@nx-testing/ts-lib"}
-    });
 
     // Set up decorator object
     const proxy: ts.LanguageService = Object.create(null);
@@ -104,6 +95,42 @@ function init(modules: { typescript: typeof ts }) {
       // @ts-expect-error - JS runtime trickery which is tricky to type tersely
       proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args);
     }
+    const enforceModuleBoundariesRuleName = "enforce-module-boundaries";
+
+    const linter = new TSESLint.Linter({ cwd: workspaceRoot });
+    const baseConfig = {
+      ...eslintConfig,
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2018 as const,
+        sourceType: "module" as const,
+      },
+    };
+
+    log(
+      info,
+      `RULLLL
+     
+      create: ${JSON.stringify(
+        //@ts-expect-error
+        nxEslintPlugin["rules"][enforceModuleBoundariesRuleName].create
+      )}
+      ${JSON.stringify(
+        //@ts-expect-error
+        nxEslintPlugin["rules"][enforceModuleBoundariesRuleName]
+      )} 
+      `
+      // ${JSON.stringify(nxEslintPlugin)}
+    );
+
+    linter.defineParser("@typescript-eslint/parser", parser);
+    linter.defineRule(
+      `${enforceModuleBoundariesRuleName}`,
+      //@ts-expect-error
+      nxEslintPlugin["rules"][enforceModuleBoundariesRuleName]
+    );
+
+    log(info, `RULLLS: ${JSON.stringify([...linter.getRules().values()])}`);
 
     // Remove specified entries from completion list
     proxy.getCompletionsAtPosition = (fileName, position, options) => {
@@ -113,40 +140,68 @@ function init(modules: { typescript: typeof ts }) {
         options
       );
 
+      if (!prior) return;
+
       const projectJsonPath = fileName
         .split("src")[0]
         .concat("project.json") as ProjectJsonPath;
 
-      //Info 1196 [22:20:24.814] projectJsonPath: /home/akisarou/nx-testing/apps/nx-testing/project.json
-      info.project.projectService.logger.info(
-        `projectJsonPath: ${projectJsonPath}`
+      const foundTagItem = tagsMap.get(projectJsonPath);
+
+      log(
+        info,
+        `getting completions for fileName -> ${fileName}. Project.json path: ${projectJsonPath}. Tags found: ${!!foundTagItem} -> ${JSON.stringify(
+          foundTagItem?.tags
+        )}`
       );
 
-      if (!prior) return;
-
-      const found = tagsMap.get(projectJsonPath);
-
-      info.project.projectService.logger.info(
-        `FOUND: ${JSON.stringify(found)}`
-      );
-
-      info.project.projectService.logger.info(
-        `fileName: ${fileName}, ${JSON.stringify(options)}`
-      );
-
-      info.project.projectService.logger.info(`isSame: ${!!found}`);
+      if (!foundTagItem) {
+        return prior;
+      }
 
       prior.entries = prior.entries.filter((e) => {
-        info.project.projectService.logger.info(`Entry: ${JSON.stringify(e)}`);
-        // @nx-testing/ts-lib, app:assistant-volunteer
-        //{"@nx-testing/ts-lib":["libs/ts-lib/src/index.ts"]}
+        const canBeImported = e.kindModifiers === "export";
 
-        const isExport = e.kindModifiers === "export";
+        if (canBeImported) {
+          const failures = linter.verify(
+            `import {${e.name}} from "${e.source}";`,
+            baseConfig,
+            fileName
+          );
 
-        // Info 6416 [19:07:11.002] Entry: {"name":"useEffect","kind":"function","kindModifiers":"declare","sortText":"16","source":"react","hasAction":true,"sourceDisplay":[{"text":"react","kind":"text"}],"data":{"exportName":"useEffect","exportMapKey":"useEffect|1916|","moduleSpecifier":"react","fileName":"/home/akisarou/vite-project/node_modules/@types/react/ts5.0/index.d.ts"}}
-        // Info 13162[19:09:27.841] Entry: {"name":"Mine","kind":"function","kindModifiers":"export","sortText":"16","source":"./mine.ts","hasAction":true,"sourceDisplay":[{"text":"./mine.ts","kind":"text"}],"data":{"exportName":"Mine","exportMapKey":"Mine|9377|","moduleSpecifier":"./mine.ts","fileName":"/home/akisarou/vite-project/src/mine.ts"}}
-        // Info 1383 [19:28:56.892] Entry: {"name":"tsLib","kind":"function","kindModifiers":"export","sortText":"16","source":"@nx-testing/ts-lib","hasAction":true,"sourceDisplay":[{"text":"@nx-testing/ts-lib","kind":"text"}],"data":{"exportName":"tsLib","exportMapKey":"tsLib|3683|","moduleSpecifier":"@nx-testing/ts-lib","fileName":"/home/akisarou/nx-testing/libs/ts-lib/src/index.ts"}}
-        return !!found;
+          log(
+            info,
+            `entry for fileName -> ${fileName} -> name: ${e.name} source: ${
+              e.source
+            }. isExport: ${canBeImported} sss: import '${
+              e.source
+            }' e.data.fileName: ${e.data?.fileName} shape: ${JSON.stringify(e)}
+            linter messages: ${JSON.stringify(failures)}
+            `
+          );
+          return failures.length === 0;
+          // info.project.projectService.logger.info(
+          //   `ENTRY is Export: (${found.tags.some((t) => t === e.source)})
+          //   source: ${JSON.stringify(found.source)}
+          //   tags: ${JSON.stringify(found.tags)}
+          //   entry: ${JSON.stringify(e)}`
+          // );
+          // for (const [_, value] of tagsMap) {
+          //   const isSameSource = value.source === e.source;
+          //   if (isSameSource) {
+          //     log(info, `Same source: ${value.source}`);
+          //     return true;
+          //   }
+          //   // return value.tags.some((t) => {
+          //   //   return foundTagItem.tags.some((ft) => {
+          //   //     log(info, `t: ${t} - ft: ${ft}`);
+          //   //     return ft === t;
+          //   //   });
+          //   // });
+          // }
+        }
+
+        return true;
       });
 
       return prior;
